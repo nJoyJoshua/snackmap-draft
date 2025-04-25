651d256316df98647f7c3cf0d4e49fb52d12fdcb
nJoyJoshua <johua.freitag@gmail.com>
2025-04-25 12:50:52 +0200

    Initial Commit

---
+++ SnackMapEntwurf/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SnackMapEntwurf
+//
+//  Created by Merry on 25.04.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SnackMapEntwurf/SnackMapEntwurfApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnackMapEntwurfApp.swift
+//  SnackMapEntwurf
+//
+//  Created by Merry on 25.04.25.
+//
+
+import SwiftUI
+
+@main
+struct SnackMapEntwurfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
